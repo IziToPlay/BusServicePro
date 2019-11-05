76b92cd322bc7368f42c94cf9fd5badf8b898423
JosLui <joslui1720082@gmail.com>
2019-11-05 01:04:17 -0500

    Avanzar mostrar mensaje de cupon de descuento obsequiado

--- src/main/java/com/cruzSolar/controller/TicketController.java
+++ src/main/java/com/cruzSolar/controller/TicketController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,238 +29,211 @@ public class TicketController {
 
 	@Autowired
 	private TicketService ticketService;
-
+	
 	@Autowired
 	private ClientService clientService;
-
+	
 	@Autowired
 	private SeatService seatService;
-
+	
 	@Autowired
 	private TripService tripService;
-
+	
 	@Autowired
 	private CouponService couponService;
-
+	
 	@Autowired
 	private ClientController clientController;
-
+	
 	@Autowired
 	private CouponController couponController;
-
+	
 	private Client client;
+	private Coupon coupon;
 	private Trip trip;
 	private Trip tripp;
-	private Ticket ticketSelect;
 	List<Ticket> tickets;
+	List<Coupon> coupons;
 	private double amountTicket;
-	private int counter = 0;
-
+	private int counter=0;
+	private long i=0;
+	
 	@GetMapping("/list")
-	public String showAllTickets(Model model) throws Exception {
-		model.addAttribute("tickets", ticketService.getAllReservedTickets());
-		// calculareAmountTickets(model);
-		return "tickets/list";
-	}
-
+    public String showAllTickets(Model model) throws Exception {
+        model.addAttribute("tickets", ticketService.getAllReservedTickets());
+       // calculareAmountTickets(model);
+        return "tickets/list";
+    }
+	
+	
+	
 	@GetMapping("/listBoughtTickets")
-	public String showAllBoughtTickets(Model model) throws Exception {
-		model.addAttribute("tickets", ticketService.getAllBoughtTickets());
-		return "tickets/listBoughtTickets";
-	}
-
+    public String showAllBoughtTickets(Model model) throws Exception {
+        model.addAttribute("tickets", ticketService.getAllBoughtTickets());
+        return "tickets/listBoughtTickets";
+    }
+	
 	@GetMapping("/new/{id}")
-	public String newTicketForm(@PathVariable("id") long id, Model model) throws Exception {
+	 public String newTicketForm(@PathVariable("id") long id, Model model) throws Exception{
 		model.addAttribute("ticket", new Ticket());
-		client = new Client();
+		client=new Client();
 		List<Client> clients = clientService.getAll();
-		model.addAttribute("clients", clients);
-		trip = tripService.getOneById(id);
+		model.addAttribute("clients",clients);
+		trip=tripService.getOneById(id);
 		List<Seat> seats = seatService.findAllSeatsAvailables(id);
-		model.addAttribute("seats", seats);
+		model.addAttribute("seats",seats);
 		return "tickets/new";
 	}
-
+	
 	@GetMapping("/searchClient")
-	public String searchClient(@RequestParam("dni") String dni, Model model) throws Exception {
+	public String searchClient(@RequestParam("dni") String dni, Model model) throws Exception{
 		model.addAttribute("ticket", new Ticket());
-		List<Client> clients = clientController.searchClient(dni, model);
+		List<Client> clients=clientController.searchClient(dni, model);
 		model.addAttribute("clients", clients);
 		List<Seat> seats = seatService.findAllSeatsAvailables(trip.getId());
-		model.addAttribute("seats", seats);
+		model.addAttribute("seats",seats);
 		return "tickets/new";
 	}
-
-	@GetMapping("/searchCoupon")
-	public String searchCoupon(@RequestParam("special") String special, Model model) throws Exception {
-
-		if (!special.isEmpty()) {
-			List<Coupon> coupons = couponController.searchCouponBySpecial(special, model);
-			Coupon cu = couponService.fetchCouponBySpe(special);
-
-			if (!coupons.isEmpty()) {
-				if (ticketSelect.getTrip().getId() == cu.getTrip().getId()) {
-					if (cu.getStatus() == false)
-						model.addAttribute("coupons", coupons);
-					else
-						model.addAttribute("info", "El cupon no esta disponible");
-				} else {
-					model.addAttribute("infor", "El cupon no corresponde a este viaje");
-				}
-			} else {
-				model.addAttribute("info", "No existe el cupon");
-			}
-		} else
-			model.addAttribute("info", "Debe completar el campo de búsqueda");
-		return "tickets/payTicket";
-	}
-
+	
 	@GetMapping("/connect/{id}")
-	public String connectClient(@PathVariable("id") long id, Model model) throws Exception {
+	public String connectClient(@PathVariable("id") long id,Model model) throws Exception {
 		model.addAttribute("ticket", new Ticket());
-		client = clientService.getOneById(id);
-		// List<Client> clients = clientService.getAll();
-		model.addAttribute("clients", client);
+		client=clientService.getOneById(id);
+		//List<Client> clients = clientService.getAll();
+		model.addAttribute("clients",client);
 		List<Seat> seats = seatService.findAllSeatsAvailables(trip.getId());
-		model.addAttribute("seats", seats);
-		model.addAttribute("success", "Cliente seleccionado correctamente");
-
+		model.addAttribute("seats",seats);
+		model.addAttribute("success","Cliente seleccionado correctamente");
+		
 		return "tickets/new";
 	}
-
-	public List<Ticket> searchTickets(String emissionDate, Model model) {
-
-		try {
-			if (!emissionDate.isEmpty()) {
-				tickets = ticketService.fetchTicketByFechaEmission(emissionDate);
-
-				if (!tickets.isEmpty()) {
-					model.addAttribute("tickets", tickets);
-				} else {
-					model.addAttribute("info", "No existen coincidencias");
-					model.addAttribute("tickets", ticketService.getAllReservedTickets());
+	
+	public List<Ticket> searchTickets(String emissionDate, Model model){
+		
+		try {	
+			if(!emissionDate.isEmpty()) {
+				tickets=ticketService.fetchTicketByFechaEmission(emissionDate);
+				
+			if(!tickets.isEmpty()) {
+				model.addAttribute("tickets", tickets);
+			}else {
+				model.addAttribute("info", "No existen coincidencias");
+				model.addAttribute("tickets",ticketService.getAllReservedTickets());
 				}
-			} else {
+			}else {
 				model.addAttribute("info", "Debe completar el campo de búsqueda.");
-				model.addAttribute("tickets", ticketService.getAllReservedTickets());
+				model.addAttribute("tickets",ticketService.getAllReservedTickets());
 			}
-		} catch (Exception e) {
+		}catch(Exception e) {
 			model.addAttribute("Error Ticket:", e.getMessage());
-		}
+		}	
 		return tickets;
 	}
-
+	
 	@GetMapping("/searchTicket")
-	public String searchTicket(@RequestParam("emissionDate") String emissionDate, Model model) throws Exception {
-
+	public String searchTicket(@RequestParam("emissionDate") String emissionDate, Model model) throws Exception{
+		
 		model.addAttribute("ticket", new Ticket());
-		List<Ticket> tickets = searchTickets(emissionDate, model);
+		List<Ticket>tickets= searchTickets(emissionDate, model);
 		model.addAttribute("tickets", tickets);
-
-		// List<Seat> seats = seatService.findAllSeatsAvailables(trip.getId());
-		// model.addAttribute("seats",seats);
+		
+		//List<Seat> seats = seatService.findAllSeatsAvailables(trip.getId());
+		//model.addAttribute("seats",seats);
 		return "tickets/list";
-
+		
 	}
-
+	
 	@PostMapping("/save")
-	public String saveNewTicket(Ticket ticket, Model model) throws Exception {
-
-		if (client.getId() != null) {
+    public String saveNewTicket(Ticket ticket, Model model) throws Exception {
+		
+		if(client.getId()!=null) {
 			ticket.setCondition(false);
 			ticket.setPrice(trip.getPrice());
 			ticket.setTrip(trip);
 			ticket.setClient(client);
 			long id = ticketService.create(ticket);
 			return "redirect:/trips/list";
-		} else {
+		}else {
 			model.addAttribute("error", "Cliente no seleccionado");
-			model.addAttribute("clients", clientService.getAll());
+			List<Client> clients = clientService.getAll();
+			model.addAttribute("clients", clients);
 			return "tickets/new";
 		}
-	}
-
+    }
+	
 	@GetMapping("/payTicket/{id}")
-	public String payTicket(@PathVariable("id") long id, Model model) throws Exception {
-		ticketSelect = ticketService.getOneById(id);
-		Ticket ticket = ticketService.getOneById(id);
-		model.addAttribute("clients", clientService.getAll());
-		model.addAttribute("seats", seatService.findAllSeatsAvailables(ticket.getTrip().getBus().getId()));
-		model.addAttribute("trips", tripService.getAll());
-		model.addAttribute("ticket", ticket);
+	public String payTicket(@PathVariable("id") long id, Model model) throws Exception{
+		
+		Ticket ticket= ticketService.getOneById(id);
+        List<Client> clients = clientService.getAll();
+		model.addAttribute("clients",clients);
+		List<Seat> seats = seatService.findAllSeatsAvailables(ticket.getTrip().getBus().getId());
+		model.addAttribute("seats",seats);
+		model.addAttribute("ticket",ticket);
 		return "tickets/payTicket";
 	}
-
+	
+	
 	@GetMapping("/edit/{id}")
-	public String editTicketForm(@PathVariable("id") long id, Model model) throws Exception {
-
-		Ticket ticket = ticketService.getOneById(id);
-		List<Client> clients = clientService.getAll();
-		model.addAttribute("clients", clients);
+    public String editTicketForm(@PathVariable("id") long id, Model model) throws Exception {
+        
+		Ticket ticket= ticketService.getOneById(id);
+        List<Client> clients = clientService.getAll();
+		model.addAttribute("clients",clients);
 		List<Seat> seats = seatService.findAllSeatsAvailables(ticket.getTrip().getBus().getId());
-		model.addAttribute("seats", seats);
-		model.addAttribute("ticket", ticket);
-
-		return "tickets/edit";
-	}
+		model.addAttribute("seats",seats);
+		model.addAttribute("ticket",ticket);
+		
+        return "tickets/edit";
+    }
 
 	@PostMapping("/update/{id}")
-	public String updateTicket(@PathVariable("id") long id, Ticket ticket, Model model) throws Exception {
-		ticketService.update(id, ticket);
-		model.addAttribute("success", "Ticket actualizado correctamente");
-		return "redirect:/tickets/list";
-	}
-
+    public String updateTicket(@PathVariable("id") long id, Ticket ticket,Model model) throws Exception {
+        ticketService.update(id, ticket);
+        model.addAttribute("success","Ticket actualizado correctamente");
+        return "redirect:/tickets/list";    
+    }
+	
 	@GetMapping("/delete/{id}")
-	public String deleteTicket(@PathVariable("id") long id, Model model) throws Exception {
-		amountTicket -= ticketService.getOneById(id).getPrice();
+	public String deleteTicket(@PathVariable("id") long id,Model model) throws Exception {
+		amountTicket-=ticketService.getOneById(id).getPrice();
 		ticketService.delete(id);
 		model.addAttribute("success", "Ticket eliminado correctamente");
 		return "redirect:/tickets/list";
 	}
-
+	
 	@GetMapping("/buy/{id}")
 	public String buyTicket(@PathVariable("id") long id, Model model) throws Exception {
 		counter++;
-		// update condition to TRUE
+		//update condition to TRUE
 		ticketService.updateCondition(id);
-		amountTicket -= ticketService.getOneById(id).getPrice();
-
-		if (counter % 3 == 0) {
-			couponController.addCoupon();
-			model.addAttribute("info", "Cupón activado por compra de 3 tickets.");
-		} else {
-			model.addAttribute("success", "Ticket comprado correctamente");
+		amountTicket-=ticketService.getOneById(id).getPrice();
+		if(counter%3==0) {
+			i++;
+			coupon=new Coupon();
+			coupon=couponService.getOneById(i);
+			coupons.add(coupon);
+			couponController.connectCoupons(coupons);
+		    model.addAttribute("info", "Cupón activado por compra de 3 tickets."); 
+		}else {
+		model.addAttribute("success", "Ticket comprado correctamente");
 		}
-
 		return "redirect:/tickets/list";
 	}
-
-	@GetMapping("/updateStatus/{id}")
-	public String updateStatus(@PathVariable("id") long id, Model model) throws Exception {
-
-		Coupon couponFound = couponService.getOneById(id);
-		couponService.updateStatus(id);
-		model.addAttribute("tickets", ticketService.getAllReservedTickets());
-		ticketService.udpatePrice(ticketSelect.getId(),
-				(couponFound.getDiscount() / 100) * couponFound.getTrip().getPrice());
-		return "tickets/list";
-	}
-
+	
 	@PostMapping("/selectTicketToDiscount/{id}")
 	public void selectTicketToDiscount(@PathVariable("id") long id, Model model) throws Exception {
-		Ticket ticket = ticketService.getOneById(id);
+		Ticket ticket=ticketService.getOneById(id);
 		model.addAttribute("success", "Ticket seleccionado para el descuento correctamente");
-
-		// Enviar a formulario "Insert Coupon" para el viaje seleccionado, el cupon debe
-		// ser acorde al viaje, si se ingresa un cupon
-		// no valido para el viaje seleccionado entonces saldra mensaje de error
+		
+		//Enviar a formulario "Insert Coupon" para el viaje seleccionado, el cupon debe ser acorde al viaje, si se ingresa un cupon
+		//no valido para el viaje seleccionado entonces saldra mensaje de error
 	}
-
+	
 	public void calculareAmountTickets(Model model) {
 		try {
-			for (long i = 1; i <= ticketService.getAllReservedTickets().size(); i++) {
-				amountTicket += ticketService.getOneById(i).getPrice();
+			for(long i=1;i <= ticketService.getAllReservedTickets().size();i++) {
+				amountTicket+=ticketService.getOneById(i).getPrice();
 			}
 		} catch (Exception e) {
 			model.addAttribute("error", e.getStackTrace());
@@ -297,4 +271,150 @@ public class TicketController {
 	public void setClient(Client client) {
 		this.client = client;
 	}
+
+
+
+	public TripService getTripService() {
+		return tripService;
+	}
+
+
+
+	public void setTripService(TripService tripService) {
+		this.tripService = tripService;
+	}
+
+
+
+	public CouponService getCouponService() {
+		return couponService;
+	}
+
+
+
+	public void setCouponService(CouponService couponService) {
+		this.couponService = couponService;
+	}
+
+
+
+	public ClientController getClientController() {
+		return clientController;
+	}
+
+
+
+	public void setClientController(ClientController clientController) {
+		this.clientController = clientController;
+	}
+
+
+
+	public CouponController getCouponController() {
+		return couponController;
+	}
+
+
+
+	public void setCouponController(CouponController couponController) {
+		this.couponController = couponController;
+	}
+
+
+
+	public Coupon getCoupon() {
+		return coupon;
+	}
+
+
+
+	public void setCoupon(Coupon coupon) {
+		this.coupon = coupon;
+	}
+
+
+
+	public Trip getTrip() {
+		return trip;
+	}
+
+
+
+	public void setTrip(Trip trip) {
+		this.trip = trip;
+	}
+
+
+
+	public Trip getTripp() {
+		return tripp;
+	}
+
+
+
+	public void setTripp(Trip tripp) {
+		this.tripp = tripp;
+	}
+
+
+
+	public List<Ticket> getTickets() {
+		return tickets;
+	}
+
+
+
+	public void setTickets(List<Ticket> tickets) {
+		this.tickets = tickets;
+	}
+
+
+
+	public List<Coupon> getCoupons() {
+		return coupons;
+	}
+
+
+
+	public void setCoupons(List<Coupon> coupons) {
+		this.coupons = coupons;
+	}
+
+
+
+	public double getAmountTicket() {
+		return amountTicket;
+	}
+
+
+
+	public void setAmountTicket(double amountTicket) {
+		this.amountTicket = amountTicket;
+	}
+
+
+
+	public int getCounter() {
+		return counter;
+	}
+
+
+
+	public void setCounter(int counter) {
+		this.counter = counter;
+	}
+
+
+
+	public long getI() {
+		return i;
+	}
+
+
+
+	public void setI(long i) {
+		this.i = i;
+	}
+
+
 }
